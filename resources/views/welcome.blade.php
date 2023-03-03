0930d30bc54193bc4e8606e3c14c2e6b733caa10
abdusaziz <abdusaziz@gmail.com>
2023-03-03 19:19:43 +0600

    simple update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
             {{ session('status') }}
         </div>
     @endif
-    <div class="h-100 p-5 text-white bg-dark rounded-3 shadow">
+    <div class="h-100 p-5 text-white bg-dark rounded-3 shadow mt-5">
         <h1 class="display-4">Hello, {{ Auth::user()->name ?? 'Guest!' }}</h1>
         <p>{{ __('You are logged in!') }} You may use it to make todo list & every todo list have seperate task list.</p>
         <a href="{{ route('todo.index') }}" class="btn btn-outline-light" type="button">Todo Page</a>
